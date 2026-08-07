561988e3425de76120b38debc112b93eef8dce4f
Ahj-6 <hj.armaghan@gmail.com>
2026-08-07 13:21:09 +0330

    Compelete Header - Responsive & Hamburger Menu

--- tohinoor-frontend/src/components/common/LoginButton/LoginButton.jsx
+++ tohinoor-frontend/src/components/common/LoginButton/LoginButton.jsx
@@ -2,12 +2,12 @@ import "./LoginButton.css";
 import { BiSolidUser } from "../../../icons";
 
 export default function LoginButton() {
-    return (
-        <button className="login-button">
-
-            <span className="login-button__text">
-                ورود اعضا  <BiSolidUser className="login-button__icon" />
-            </span>
-        </button>
-    );
+  return (
+    <button className="login-button">
+      <span className="login-button__content">
+        <span className="login-button__label">ورود اعضا</span>
+        <BiSolidUser className="login-button__icon" />
+      </span>
+    </button>
+  );
 }
--- tohinoor-frontend/src/components/layout/Header/Header.jsx
+++ tohinoor-frontend/src/components/layout/Header/Header.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import "./Header.css";
 import "./Header-responsive.css";
 
@@ -6,22 +8,59 @@ import LoginButton from "../../common/LoginButton";
 import StarField from "../../common/StarField";
 
 import HeaderNav from "./HeaderNav";
+import { headerMenu } from "../../../constants/headerMenu";
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
   return (
     <StarField variant="header">
       <header className="header">
+        {/* Hamburger */}
+        <button
+          className="header__menu"
+          type="button"
+          onClick={toggleMenu}
+          aria-label={isMenuOpen ? "بستن منو" : "باز کردن منو"}
+          aria-expanded={isMenuOpen}
+        >
+          {isMenuOpen ? "✕" : "☰"}
+        </button>
 
-        <a className="header__logo">
+        {/* Logo */}
+        <a className="header__logo" href="/">
           <Logo size={62} />
         </a>
 
+        {/* Desktop Navigation */}
         <HeaderNav />
 
+        {/* Login */}
         <div className="header__actions">
           <LoginButton />
         </div>
 
+        {/* Mobile Menu */}
+        <nav
+          className={`header__mobile-menu ${
+            isMenuOpen ? "header__mobile-menu--open" : ""
+          }`}
+        >
+          {headerMenu.map((item) => (
+            <a
+              key={item.path}
+              href={item.path}
+              className="header__mobile-link"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              {item.title}
+            </a>
+          ))}
+        </nav>
       </header>
     </StarField>
   );
